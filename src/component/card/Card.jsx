c8a393b58e30985ff6c69d2ab39d2b5b94bd9ddb
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2022-11-02 00:41:04 +0700

    fix: minor bug onClick card component

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -15,22 +15,17 @@ export default function Card({ newsDataItem }) {
   const { savedNews, handleSaveNews, handleDeleteNews } = useCard(newsDataItem);
 
   return (
-    <a href={newsDataItem?.url} target="_blank" rel="noreferrer">
-      <Center
-        py={6}
-        _hover={{
-          cursor: "pointer",
-        }}
+    <Center py={6}>
+      <Box
+        maxW={"445"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        p={6}
+        overflow={"hidden"}
       >
-        <Box
-          maxW={"445"}
-          w={"full"}
-          bg={useColorModeValue("white", "gray.900")}
-          boxShadow={"2xl"}
-          rounded={"md"}
-          p={6}
-          overflow={"hidden"}
-        >
+        <a href={newsDataItem?.url} target="_blank" rel="noreferrer">
           <Box h={"210px"} bg={"gray.100"}>
             <Image
               objectFit="cover"
@@ -60,41 +55,42 @@ export default function Card({ newsDataItem }) {
               noOfLines={1}
               _hover={{ textDecoration: "underline" }}
             >
+              {" "}
               {newsDataItem?.title}
             </Heading>
             <Text color={"gray.500"} noOfLines={4}>
               {newsDataItem?.content}
             </Text>
           </Stack>
+        </a>
 
-          <Stack mt={2} spacing={0} fontSize={"sm"}>
-            <Text fontWeight={600}>{newsDataItem.author || "unknown"}</Text>
-            <Text color={"gray.500"}>
-              {new Date(newsDataItem.publishedAt).toLocaleDateString("en-US", {
-                day: "numeric",
-                month: "short",
-                year: "numeric",
-              })}
-            </Text>
-          </Stack>
+        <Stack mt={2} spacing={0} fontSize={"sm"}>
+          <Text fontWeight={600}>{newsDataItem.author || "unknown"}</Text>
+          <Text color={"gray.500"}>
+            {new Date(newsDataItem.publishedAt).toLocaleDateString("en-US", {
+              day: "numeric",
+              month: "short",
+              year: "numeric",
+            })}
+          </Text>
+        </Stack>
 
-          <HStack mt={2} justifyContent={"end"}>
-            {savedNews.some((e) => e.url === newsDataItem.url) ? (
-              <Button
-                h={8}
-                bgColor={"red.600"}
-                onClick={() => handleDeleteNews(newsDataItem.url)}
-              >
-                Unsave
-              </Button>
-            ) : (
-              <Button h={8} bgColor={"blue.600"} onClick={handleSaveNews}>
-                Save
-              </Button>
-            )}
-          </HStack>
-        </Box>
-      </Center>
-    </a>
+        <HStack mt={2} justifyContent={"end"}>
+          {savedNews.some((e) => e.url === newsDataItem.url) ? (
+            <Button
+              h={8}
+              bgColor={"red.600"}
+              onClick={() => handleDeleteNews(newsDataItem.url)}
+            >
+              Unsave
+            </Button>
+          ) : (
+            <Button h={8} bgColor={"blue.600"} onClick={handleSaveNews}>
+              Save
+            </Button>
+          )}
+        </HStack>
+      </Box>
+    </Center>
   );
 }
